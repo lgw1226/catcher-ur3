9784b3a40c1058075dbd0fc4a9160d4004af92ec
lgw1226 <zczc156@gmail.com>
2023-08-29 13:34:58 +0900

    Intermediate commit
    Parabola approximation is not done yet. Need to work on that.

--- catch_ur3_parabola_approx_controller.py
+++ catch_ur3_parabola_approx_controller.py
@@ -2,6 +2,8 @@ from copy import copy
 from collections import deque
 
 import numpy as np
+import torch
+import torch.optim as optim
 
 from catch_ur3.envs.catch_ur3 import CatchUR3Env
 
@@ -28,18 +30,52 @@ def tidy_key_val_print(key, val, terminal_width=80):
 
     print(f"{key}{val:>{leftover_width}.4f}")
 
-def estimate_trajectory(pos_sequence):
-
-    x = pos_sequence[:,0]
-    y = pos_sequence[:,1]
-    z = pos_sequence[:,2]
-
+def estimate_trajectory(
+    pos_sequence,
+    p1=None,  # parameters for straignt line on xy-plane
+    p2=None,  # parameters for parabola on sz-plane
+    max_iter=100,
+    alpha=0.005,
+    eps=0.001
+):
     
+    pos_sequence = torch.tensor(pos_sequence)
+    len_sequence = len(pos_sequence)
+
+    # fit a straight line on xy-plane
+    xy1 = torch.cat((pos_sequence[:,0:2], torch.ones(len_sequence, 1)), axis=1)
+
+    if not p1:  # if there's no initial guess
+        p1 = torch.randn(3, dtype=torch.float64, requires_grad=True)  # a_1 * x + a_2 * y + a_3 = 0
+    else:  # typecasting for input check
+        p1 = torch.tensor(p1, requires_grad=True)
+
+    loss_old = torch.zeros(1)
+
+    for _ in range(max_iter):
+
+        loss = torch.mean(torch.abs(xy1 @ p1) / torch.sqrt(p1[0] ** 2 + p1[1] ** 2))
+        print(loss)
+        if torch.abs(loss - loss_old) <= eps:
+            print('broken')
+            break
+        
+        loss.backward()
+        loss_old = loss
+
+        with torch.no_grad():
+            p1_new = p1 - alpha * p1.grad
+            p1 = p1_new.requires_grad_()
+
+    # projection of xy-points on the straight line
+    
+
+    # fit a parabola on sz-plane
 
 
 env = CatchUR3Env()
 
-num_episode = 5
+num_episode = 1
 len_ball_pos_sequence = 5  # number of coordinates that make up a sequence
 
 tidy_center_print('Info')
@@ -60,7 +96,7 @@ for idx_episode in range(num_episode):
         ball_pos_sequence.push(copy(info['ball_pos']))
 
         if idx_step >= len_ball_pos_sequence:
-            print(ball_pos_sequence.get_data())
+            estimate_trajectory(ball_pos_sequence.get_data())
  
 env.close()
 
